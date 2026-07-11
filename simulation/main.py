cbcf4425fa9c8acb70126172f8aa42e0ccb9711c
Debarpan Mitra <debarpanmitra1310@gmail.com>
2026-07-11 20:56:42 +0530

    Create main.py

---
+++ simulation/main.py
@@ -0,0 +1,18 @@
+#MAIN
+
+import os
+
+
+def main():
+
+    print("=" * 50)
+    print("        ANTARES v0.1")
+    print("=" * 50)
+
+    print("\nLaunching Dashboard...\n")
+
+    os.system("streamlit run app.py")
+
+
+if __name__ == "__main__":
+    main()
